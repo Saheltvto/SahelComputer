8be30d0a28e678e81cf87e687498b346fa2a7cd6
Saheltvto <120220147+Saheltvto@users.noreply.github.com>
2026-08-14 16:03:16 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // آدرس Web App که بعد از Deploy کردن Apps Script می‌گیرید را اینجا جایگزین کنید
 // مثال: https://script.google.com/macros/s/AKfycb.../exec
-const SAHEL_API_URL = "REPLACE_WITH_YOUR_DEPLOYMENT_URL";
+const SAHEL_API_URL = "https://script.google.com/macros/s/AKfycbyS2fUQG28Tpr2H2HrKZkql15UO9XJdnFjrwUk80MSQkhbk-JVODdKzLFPBE4jXdTg91g/exec";
 
 async function sahelApiCall(payload) {
   const res = await fetch(SAHEL_API_URL, {
